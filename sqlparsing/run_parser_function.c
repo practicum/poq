dd93d87111157063d275768cbb53ccf8e1774676
Someone <someone@localhost>
2012-06-10 12:42:52 -0700

    just further familiarizing myself with the code by running it and commenting it

--- sqlparsing/run_parser_function.c
+++ sqlparsing/run_parser_function.c
@@ -2,32 +2,33 @@
 
 
 /*
-** Run the parser on the given SQL string.  The parser structure is
-** passed in.  An SQLITE_ status code is returned.  If an error occurs
-** then an and attempt is made to write an error message into
-** memory obtained from sqlite3_malloc() and to make *pzErrMsg point to that
-** error message.
+** Run the parser on the given SQL string.  The parser structure is passed in.
+** An SQLITE_ status code is returned.  If an error occurs then an attempt is
+** made to write an error message into memory obtained from sqlite3_malloc() and
+** to make *pzErrMsg point to that error message.
 */
 int sqlite3RunParser(Parse *pParse, const char *zSql, char **pzErrMsg)
 {
     int nErr = 0;                   /* Number of errors encountered */
-    int i = 0;                      /* Loop counter */
+    int positionInStr = 0;          /* position in sql statement string */
     void *pEngine = 0;              /* The LEMON-generated LALR(1) parser */
     int tokenType = 0;              /* type of the next token */
-    int lastTokenParsed = -1;       /* type of the previous token */
-    const u8 enableLookaside = 0;   /* Saved value of db->lookaside.bEnabled */
+    int lastTokenTypeParsed = -1;   /* type of the previous token */
     const int mxSqlLen = 1000000;   /* Max length of an SQL string */
 
     // The maximum number of bytes in the text of an SQL statement is limited to SQLITE_MAX_SQL_LENGTH which defaults to 1000000.
 
-    pParse->rc = 0; //SQLITE_OK;
-    pParse->zTail = zSql;
-    i = 0;
     assert( pzErrMsg!=0 );
+    pParse->rc = 0; //SQLITE_OK;
+
+    // per the description of 'struct Parse', 'zTail' always holds all remaining text after the last known semicolon
+    pParse->zTail = zSql;
+
     pEngine = sqlite3ParserAlloc(malloc);
 
     if( pEngine==0 )
     {
+        pParse->rc = 8;
         return 8;//SQLITE_NOMEM;
     }
     assert( pParse->pNewTable==0 );
@@ -36,15 +37,15 @@ int sqlite3RunParser(Parse *pParse, const char *zSql, char **pzErrMsg)
     assert( pParse->nzVar==0 );
     assert( pParse->azVar==0 );
 
-    while( zSql[i]!=0 )
+    while( zSql[positionInStr] != 0 )
     {
-        assert( i>=0 );
-        pParse->sLastToken.z = &zSql[i]; // the entire string 'tail' from current position onward
+        assert( positionInStr>=0 );
+        pParse->sLastToken.z = &zSql[positionInStr]; // the entire string 'tail' from current position onward
         pParse->sLastToken.n = // the size (character count) of whichever token just got found
-            sqlite3GetToken( (unsigned char*)&zSql[i], &tokenType );
-        i += pParse->sLastToken.n;
+            sqlite3GetToken( (unsigned char*)&zSql[positionInStr], &tokenType );
+        positionInStr += pParse->sLastToken.n;
 
-        if( i>mxSqlLen )
+        if( positionInStr > mxSqlLen )
         {
             pParse->rc = 1;// SQLITE_TOOBIG;
             break;
@@ -54,35 +55,34 @@ int sqlite3RunParser(Parse *pParse, const char *zSql, char **pzErrMsg)
         {
         case TK_SPACE:
             {
-                /*
-                  if( db->u1.isInterrupted ){
-                  sqlite3ErrorMsg(pParse, "interrupt");
-                  pParse->rc = 1;// SQLITE_INTERRUPT;
-                  goto abort_parse;
-                  }*/
+                // every time the tokenizer code finds whitespace, we get here. we just ignore it.
                 break; // break from SWITCH, not the while loop...
             }
         case TK_ILLEGAL:
             {
-                //sqlite3DbFree(db, *pzErrMsg);
-                //*pzErrMsg = sqlite3MPrintf(db, "unrecognized token: \"%T\"",
-                //              &pParse->sLastToken);
+                // this will happen if you use invalid tokens. for example: this is ok --> "!=" but this is not --> "!!"
+                pParse->rc = -1;
+                printf("unrecognized token: %s\n", pParse->sLastToken.z );
                 nErr++;
                 goto abort_parse;
             }
         case TK_SEMI:
             {
-                pParse->zTail = &zSql[i];
+                // found a SEMICOLON (;)
+
+                // Per the description of 'struct Parse', 'zTail' always holds all remaining text after the last known semicolon
+                pParse->zTail = &zSql[positionInStr];
                 /* Fall thru into the default case */
             }
         default:
             {
                 sqlite3Parser(pEngine, tokenType, pParse->sLastToken, pParse);
-                lastTokenParsed = tokenType;
+                lastTokenTypeParsed = tokenType;
 
-                //if( pParse->rc!=SQLITE_OK ){
-                //  goto abort_parse;
-                //}
+                if( pParse->rc != 0 )
+                {
+                    goto abort_parse;
+                }
 
                 break; // break from SWITCH, not the while loop...
             }
@@ -93,10 +93,10 @@ int sqlite3RunParser(Parse *pParse, const char *zSql, char **pzErrMsg)
 
  abort_parse:
     /*
-      if( zSql[i]==0 && nErr==0 && pParse->rc==SQLITE_OK ){
-      if( lastTokenParsed!=TK_SEMI ){
+      if( zSql[positionInStr]==0 && nErr==0 && pParse->rc==SQLITE_OK ){
+      if( lastTokenTypeParsed!=TK_SEMI ){
       sqlite3Parser(pEngine, TK_SEMI, pParse->sLastToken, pParse);
-      pParse->zTail = &zSql[i];
+      pParse->zTail = &zSql[positionInStr];
       }
       sqlite3Parser(pEngine, 0, pParse->sLastToken, pParse);
       }
