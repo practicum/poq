9330aa645d260980a03b1cf5e33a1916f1b4af52
Someone <someone@localhost>
2012-08-11 19:46:32 -0700

    another sample SQL query that runs against the 'camping' database

---
+++ sample_databases/camping/sample_query_check_barcode.sql
@@ -0,0 +1,68 @@
+
+-- given a barcode, we use this query to find out whether it is a
+-- full-membership barcode or a trial membership barcode, and we also
+-- find out how many vehicles it supports and for which amenities
+-- access level.  We might also discover that the barcode is no known
+-- in the system at all.
+
+SELECT
+      cnt
+    , a_id
+    , a_id_check
+    , bc_type
+    , bc_type_check
+    , access.amenities_access_level_name
+FROM
+    (
+
+        SELECT
+              count(amenities_id)                as cnt
+            , coalesce( min(amenities_id), 1 )   as a_id
+            , coalesce( max(amenities_id), 1 )   as a_id_check
+            , coalesce( min(barcode_type), 'x' ) as bc_type
+            , coalesce( max(barcode_type), 'x' ) as bc_type_check
+        FROM
+            AmenitiesAccessBarcode code
+        JOIN
+            Purchase pur
+        ON
+                code.barcode_string = @barcode   -- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        AND
+                code.barcode_string = pur.barcode_string
+        AND
+                coalesce(pur.canceled,0) = 0
+
+    UNION ALL
+
+        SELECT
+              count(amenities_id)                 as cnt
+            , coalesce( min(amenities_id), 1 )    as a_id
+            , coalesce( max(amenities_id), 1 )    as a_id_check
+            , coalesce( min(barcode_type), 'x' )  as bc_type
+            , coalesce( max(barcode_type), 'x' )  as bc_type_check
+        FROM
+            AmenitiesAccessBarcode code
+        JOIN
+            GuestTrialPeriod trial
+        ON
+                code.barcode_string = @barcode   -- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        AND
+                code.barcode_string = trial.barcode_string
+        AND
+                coalesce(trial.canceled,0) = 0
+
+    ) unified
+
+LEFT JOIN
+    AmenitiesAccessType access
+ON
+    coalesce( unified.a_id , 1 ) = access.amenities_id
+
+ORDER BY cnt DESC;
+
+
+
+--  TO BE TESTED: query always returns exactly two rows.
+--  TO BE TESTED: second row always has cnt = 0. (should always be the 2nd row due to the ORDER BY)
+--  TO BE TESTED: any row with zero should have 'x'
+--  TO BE TESTED: min and max should match on a per-row basis
